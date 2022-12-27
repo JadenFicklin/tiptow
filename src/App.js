2374eac7352f7641237dbac8290fc48d5e362de4
JadenFicklin <jadendficklin@gmail.com>
2022-12-26 22:26:53 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
+import Forum from "./components/Forum";
+import Shop from "./components/Shop";
+import Events from "./components/Events";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <>
-      <Landing />
+      <Router>
+        <Landing />
+        <Forum />
+        <Shop />
+        <Events />
+
+        <Routes>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "../styles/Nav.css";
 import { AiFillCaretDown } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <>
       <div className="nav-outer">
         <div className="nav-inner">
-          <div className="logo"></div>
-          <div className="forum">FORUM</div>
-          <div className="shop">SHOP</div>
-          <div className="events">EVENTS</div>
-          <div className="sign-in">
-            SIGN IN <AiFillCaretDown className="icon" />
-          </div>
+          <Link to="/">
+            <div className="logo"></div>
+          </Link>
+          <Link to="/forum">
+            <div className="forum">FORUM</div>
+          </Link>
+          <Link to="/shop">
+            <div className="shop">SHOP</div>
+          </Link>
+          <Link to="/events">
+            <div className="events">EVENTS</div>
+          </Link>
+          <Link to="/signin">
+            <div className="sign-in">
+              SIGN IN <AiFillCaretDown className="icon" />
+            </div>
+          </Link>
         </div>
       </div>
     </>
